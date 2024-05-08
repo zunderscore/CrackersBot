37f7f4b80e3c45e66cd48acf24a76ce69fbc277d
Zack Williamson <zwilliamson@outlook.com>
2024-05-08 14:30:41 -0400

    Add guild ID variable

---
+++ src/CrackersBot.Core/Variables/Discord/GuildIdVariable.cs
@@ -0,0 +1,6 @@
+namespace CrackersBot.Core.Variables.Discord
+{
+    [VariableToken(CommonNames.DISCORD_GUILD_ID)]
+    [VariableDescription("The Discord guild ID")]
+    public class GuildIdVariable : VariableBase { }
+}
